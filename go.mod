5d34af82e1f586b6ac6c47754453b704e0bd39a2
Terng <terng.pongdilok@gmail.com>
2021-10-13 22:10:13 +0700

    refactor_conn, add cli task management

--- go.mod
+++ go.mod
@@ -3,10 +3,14 @@ module github.com/Terng/GoWebApp
 go 1.17
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/russross/blackfriday/v2 v2.0.1 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/urfave/cli/v2 v2.3.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
@@ -15,4 +19,5 @@ require (
 	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073 // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/text v0.3.5 // indirect
+	gopkg.in/gookit/color.v1 v1.1.6 // indirect
 )
--- main.go
+++ main.go
@@ -2,29 +2,117 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"errors"
+
 	"log"
+	"os"
 	"time"
 
+	"github.com/urfave/cli/v2"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"gopkg.in/gookit/color.v1"
 )
 
-func main() {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017/"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Disconnect(ctx)
+var collection *mongo.Collection
+var ctx = context.TODO()
 
-	db, _ := client.ListDatabaseNames(ctx, bson.M{})
+func main() {
+	app := &cli.App{
+		Name:  "tasker",
+		Usage: "A simple CLI Program to manage your tasks",
+		Commands: []*cli.Command{
+			{
+				Name:    "add",
+				Aliases: []string{"a"},
+				Usage:   "add a task to the list",
+				Action: func(c *cli.Context) error {
+					str := c.Args().First()
+					if str == "" {
+						return errors.New("Cannot add an empty task")
+					}
+					task := &Task{
+						ID:        primitive.NewObjectID(),
+						CreatedAt: time.Now(),
+						UpdateAt:  time.Now(),
+						Text:      str,
+						Completed: false,
+					}
+					return createTask(task)
+				},
+			},
+		},
+	}
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("connected", db)
+}
+
+func init() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	collection = client.Database("tasker").Collection("tasks")
+}
+
+type Task struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	CreatedAt time.Time          `bson:"created_at"`
+	UpdateAt  time.Time          `bson:"updated_at"`
+	Text      string             `bson:"text"`
+	Completed bool               `bson:"completed"`
+}
+
+func createTask(task *Task) error {
+	_, err := collection.InsertOne(ctx, task)
+	return err
+}
+
+func getAll() ([]*Task, error) {
+	filter := bson.D{{}}
+	return filterTasks(filter)
+}
+
+func filterTasks(filter interface{}) ([]*Task, error) {
+	var tasks []*Task
+
+	cur, err := collection.Find(ctx, filter)
+	if err != nil {
+		return tasks, err
+	}
+	for cur.Next(ctx) {
+		var t Task
+		err := cur.Decode(&t)
+		if err != nil {
+			return tasks, err
+		}
+		tasks = append(tasks, &t)
+	}
+	if err := cur.Err(); err != nil {
+		return tasks, err
+	}
+	cur.Close(ctx)
+	if len(tasks) == 0 {
+		return tasks, mongo.ErrNoDocuments
+	}
+	return tasks, nil
+}
+
+func printTask(tasks []*Task) {
+	for i, v := range tasks {
+		if v.Completed {
+			color.Green.Printf("%d: %s\n", i+1, v.Text)
+		} else {
+			color.Yellow.Printf("%d: %s\n", i+1, v.Text)
+		}
+	}
 }
